3b28124e354ef22b7db8af5b01cd0d961f630103
Nkem-wey-sabi <kemcho.ogor@gmail.com>
2021-08-18 03:45:02 -0700

    added first HNGi8 git task

---
+++ Print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the void function
+*
+* Description: using the main function
+* this program prints "Prints my name"
+* Return: 0
+*/
+int main(void)
+{
+	printf("Ogor Nkemchor\n");
+	return (0);
+}
